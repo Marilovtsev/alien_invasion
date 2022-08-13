f8a2feceb18ae49628a5031cf4be0bd9326ca659
Marilovtsev <dimarilovcev@gmail.com>
2022-08-13 18:47:22 +0300

    Create init method

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,8 +1,13 @@
 import sys
 import pygame
 
+
 class AlienInvasion:
     """Клас для керування ресурсами та поведінкою гри"""
 
     def __init__(self):
         """Ініціалізує гру і створює игрові ресурси"""
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Invasion")
