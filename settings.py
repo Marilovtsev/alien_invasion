e06ce15ccc9d1c7d3ed7e946722a6430879c1c54
Marilovtsev <dimarilovcev@gmail.com>
2022-08-16 21:00:03 +0300

    A class to store all Alien Invasion game settings.

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings():
+    """Класс для зберігання усіх налаштувань гри Alien Invasion"""
+
+    def __init__(self):
+        """Ініціалізує налаштування гри"""
+        #Monitor settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        selg.bg_color = (230, 230, 230)
