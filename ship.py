8014328c96eb808e0d7782499b0f61d0720ccc8f
Marilovtsev <dimarilovcev@gmail.com>
2022-08-16 21:37:24 +0300

    Add the Ship loading

--- ship.py
+++ ship.py
@@ -7,3 +7,9 @@ class Ship():
         """Ініціалізує корабель та задає його початкову позицію"""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+
+        #Завантажує зображення корабля та отримує прямокутник
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+        #Кожен новий корабель з'ялвяється у нижньому куті екрану
+        self.rect.midbottom = self.screen_rect.midbottom
