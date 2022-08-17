5b2f62590e07cdf2c343d1e00a44229e1c8c0358
Marilovtsev <dimarilovcev@gmail.com>
2022-08-17 21:43:26 +0300

    Add action KEYDOWN (right)

--- alien_invasion.py
+++ alien_invasion.py
@@ -32,6 +32,10 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    # Переммістити корабель вліво
+                    self.ship.rect.x += 1
 
     def _update_screen(self):
         """Оновлює зображення на екрані та відображає новий екран"""
