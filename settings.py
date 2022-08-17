ebb206ad178a6b5fae268a09d52b57474a453a3a
Marilovtsev <dimarilovcev@gmail.com>
2022-08-17 21:31:47 +0300

    Make background a blue

--- settings.py
+++ settings.py
@@ -6,4 +6,4 @@ class Settings():
         #Monitor settings
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (230, 230, 230)
+        self.bg_color = (0, 0, 250)
