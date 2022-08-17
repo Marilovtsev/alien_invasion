5e6b33a9c33bb8161d2b2f0d748be7279196e89b
Marilovtsev <dimarilovcev@gmail.com>
2022-08-17 21:59:04 +0300

    The background of the ship image is made transparent

--- settings.py
+++ settings.py
@@ -6,4 +6,4 @@ class Settings():
         #Monitor settings
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (100, 200, 250)
+        self.bg_color = (0, 200, 250)
