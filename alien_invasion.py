0bdffd8613adfae9e746846c6957d19f2a217ee2
Marilovtsev <dimarilovcev@gmail.com>
2022-08-23 18:53:23 +0300

    Refactoring _check_events() to Handle KEYDOWN Events

--- alien_invasion.py
+++ alien_invasion.py
@@ -34,16 +34,21 @@ class AlienInvasion:
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
-
+                self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False
+                self._check_keyup_events(event)
+                # if event.key == pygame.K_RIGHT:
+                #     self.ship.moving_right = False
+                # elif event.key == pygame.K_LEFT:
+                #     self.ship.moving_left = False
+
+    def _check_keydown_events(self, event):
+        """Реагує на натискання клавіш."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+
 
 
     def _update_screen(self):
