ea483966de1b2c50f11e814c46045e6f7077e629
Marilovtsev <dimarilovcev@gmail.com>
2022-08-24 21:32:04 +0300

    Some fix

--- rocket_set.py
+++ rocket_set.py
@@ -6,7 +6,7 @@ class Settings():
         # Monitor settings
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (0, 200, 250)
+        self.bg_color = (15, 200, 250)
 
         # Ship settings
         self.ship_speed = 1.5
