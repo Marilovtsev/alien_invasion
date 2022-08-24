fdc8e045f4f6cb243704dfb3cbca0c70b738a623
Marilovtsev <dimarilovcev@gmail.com>
2022-08-24 20:13:15 +0300

    Some fix

--- settings.py
+++ settings.py
@@ -3,10 +3,10 @@ class Settings():
 
     def __init__(self):
         """Ініціалізує налаштування гри"""
-        #Monitor settings
+        # Monitor settings
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (0, 200, 250)
 
-        #Ship settings
+        # Ship settings
         self.ship_speed = 1.5
