cc0a5f48f3daa4cb8dc47aad6a917da3e352f03a
Marilovtsev <dimarilovcev@gmail.com>
2022-08-25 20:57:36 +0300

    Some fix

--- rocket.py
+++ rocket.py
@@ -14,7 +14,7 @@ class Rocket():
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
         self.center = float(self.rect.centerx)
-        self.cemter = float(self.rect.centery)
+        self.center = float(self.rect.centery)
         self.moving_right = False
         self.moving_left = False
         self.moving_height = False
@@ -28,13 +28,13 @@ class Rocket():
         """Оновлює позицію корабля з урахуванням флагів"""
         # Оновлює атрибут х, не rect
         if self.moving_right and self.rect.right < self.screen_rect.right:
-            self.x += self.settings.rocket_speed
+            self.center += self.ai_settings.rocket_speed
         if self.moving_left and self.rect.left > 0:
-            self.x -= self.settings.rocket_speed
+            self.center -= self.ai_settings.rocket_speed
         if self.moving_height and self.rect.top > self.screen_rect.top:
-            self.rect.centery -= self.settings.rocket_speed
+            self.rect.centery -= self.ai_settings.rocket_speed
         if self.moving_width and self.rect.bottom < self.screen_rect.midbottom:
-            self.rect.centery += self.settings.rocket_speed
+            self.rect.centery += self.ai_settings.rocket_speed
         self.rect.centerx = self.center
 
         # Оновлення атрибуту rect на основі self.x
