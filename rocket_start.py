2ebea3688ed2d87eb641be688bee3abe2fcedfc7
Marilovtsev <dimarilovcev@gmail.com>
2022-08-29 20:12:11 +0300

    Some fix

--- rocket_start.py
+++ rocket_start.py
@@ -28,46 +28,6 @@ class RocketStart:
             self._update_screen()
         # При кожному проході циклу екран перемальовуєтся
 
-    def _check_events(self):
-        """Обробляє натискання клавіатури та події миші"""
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-            elif event.type == pygame.KEYDOWN:
-                self._check_keydown_events(event)
-            elif event.type == pygame.KEYUP:
-                self._check_keyup_events(event)
-
-    def _check_keydown_events(self, event):
-        """Реагує на натискання клавіш."""
-        if event.key == pygame.K_RIGHT:
-            self.rocket.moving_right = True
-        elif event.key == pygame.K_LEFT:
-            self.rocket.moving_left = True
-        elif event.key == pygame.K_UP:
-            self.rocket.moving_height = True
-        elif event.key == pygame.K_DOWN:
-            self.rocket.moving_width = True
-        elif event.key == pygame.K_q:
-            sys.exit()
-
-    def _check_keyup_events(self, event):
-        """Реагує на відпускання клавіш."""
-        if event.key == pygame.K_RIGHT:
-            self.rocket.moving_right = False
-        elif event.key == pygame.K_LEFT:
-            self.rocket.moving_left = False
-        elif event.key == pygame.K_UP:
-            self.rocket.moving_height = False
-        elif event.key == pygame.K_DOWN:
-            self.rocket.moving_width = False
-
-    def _update_screen(self):
-        """Оновлює зображення на екрані та відображає новий екран"""
-        self.screen.fill(self.settings.bg_color)
-        self.rocket.blitme()
-
-        pygame.display.flip()
 
 
 if __name__ == '__main__':
