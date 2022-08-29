2283d1c24f848f299466be08b5cf8b879c3ec04d
Marilovtsev <dimarilovcev@gmail.com>
2022-08-29 20:31:52 +0300

    Some fix

--- rocket_start.py
+++ rocket_start.py
@@ -6,3 +6,17 @@ from rocket_set import Settings
 
 
 def run_game():
+    pygame.init()
+    settings = Settings()
+    screen = pygame.display.set_mode((
+        settings.screen_width, settings.screen_height))
+    pygame.display.set_caption('My Rocket')
+
+    rocket = Rocket(settings, screen)
+
+    while True:
+        func.check_events(rocket)
+        rocket.update()
+        func.update_screen(settings, screen, rocket)
+
+run_game()
