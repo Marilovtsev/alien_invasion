36ed2887989fb236cbb38b22a3761ec0483f6e70
Marilovtsev <dimarilovcev@gmail.com>
2022-08-31 19:55:49 +0300

    Add limit of bullets in class Settings

--- settings.py
+++ settings.py
@@ -7,6 +7,7 @@ class Settings():
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (0, 200, 250)
+        self.bullet_allowed = 3
 
         # Ship settings
         self.ship_speed = 1.5
