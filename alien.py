feee2bd20978cac37f2801fab0eb976a31165a96
Marilovtsev <dimarilovcev@gmail.com>
2022-09-07 21:26:38 +0300

    Aliens spawn location

--- alien.py
+++ alien.py
@@ -10,13 +10,13 @@ class Alien(Sprite):
         super().__init__()
         self.screen = ai_game.screen
 
-        #Завантаження зображення прибульця та призначення атрибуту rect.
+        # Завантаження зображення прибульця та призначення атрибуту rect.
         self.image = pygame.image.load('images/alien.bmp')
         self.rect = self.image.get_rect()
 
-        #Кожен новий прибулець з'являється у лівому верхньому куті екрану.
+        # Кожен новий прибулець з'являється у лівому верхньому куті екрану.
         self.rect.x = self.rect.width
         self.rect.y = self.rect.height
 
-        #Закріплення точної горизонтальної позиції прибульця.
+        # Закріплення точної горизонтальної позиції прибульця.
         self.x = float(self.rect.x)
