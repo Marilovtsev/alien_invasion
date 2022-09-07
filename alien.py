e6cf77d7f1e8b4090b7acde9857538fe4f7f42d4
Marilovtsev <dimarilovcev@gmail.com>
2022-09-07 21:07:02 +0300

    Create Alien class

---
+++ alien.py
@@ -0,0 +1,8 @@
+import  pygame
+from  pygame.sprite import Sprite
+
+class Alien(Sprite):
+    """Клас, один прибулець."""
+
+    def __init__(self, ai_game):
+        """Ініціалізує прибульця та задає його початкову позицію."""
