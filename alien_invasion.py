1f6d7492548fd3636763cbc9835c92ed43c758cc
Marilovtsev <dimarilovcev@gmail.com>
2022-09-13 21:28:57 +0300

    Upgrade _create_alien method

--- alien_invasion.py
+++ alien_invasion.py
@@ -108,6 +108,7 @@ class AlienInvasion:
         alien_width, alien_height = alien.rect.size
         alien.x = alien_width + 2 * alien_width * alien_number
         alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
 
     def _update_screen(self):
