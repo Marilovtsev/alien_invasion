69fba7a7f55b2ddf73091bbf85613dff35bb391d
Marilovtsev <dimarilovcev@gmail.com>
2022-09-14 20:26:23 +0300

    Update update method

--- alien.py
+++ alien.py
@@ -23,14 +23,16 @@ class Alien(Sprite):
         # Закріплення точної горизонтальної позиції прибульця.
         self.x = float(self.rect.x)
 
-   def chek_edges(self):
-       """Повертає True якщо прибулець знаходиться біля краю екрана"""
-       screen_rect = self.screen.get_rect()
-       if self.rect.right >=screen_rect or self.rect.left <= 0:
-           return True
+
+    def chek_edges(self):
+        """Повертає True якщо прибулець знаходиться біля краю екрана"""
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect or self.rect.left <= 0:
+            return True
+
 
     def update(self):
         """Переміщує прибульця вліо і вправо"""
         self.x += (self.settings.alien_speed *
-                   self.settings.fleet_direction)
+                self.settings.fleet_direction)
         self.rect.x = self.x
