d29a40553670dfd19816b8cefda438938a9ae33c
Marilovtsev <dimarilovcev@gmail.com>
2022-09-14 19:08:01 +0300

    update settings

--- settings.py
+++ settings.py
@@ -9,7 +9,9 @@ class Settings():
         self.bg_color = (0, 30, 80)
         self.bullet_allowed = 3
         self.alien_speed = 0.1
-
+        self.fleet_drop_speed = 10
+        # fleet direction = 1 означає рух вправо; f -1 - вліво
+        self.fleet_direction = 1
         # Ship settings
         self.ship_speed = 1.5
 
