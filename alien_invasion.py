eb1f0b234c97c241e0a73da87027ca975c438670
Marilovtsev <dimarilovcev@gmail.com>
2022-09-15 22:06:16 +0300

    Update

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,7 +1,10 @@
 import sys
+from time import sleep
+
 import pygame
 
 from settings import Settings
+from game_stats import GameStats
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
