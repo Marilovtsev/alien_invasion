88c02c545f58f9b557f4c88c8b5c0fe5901b7fd3
Marilovtsev <dimarilovcev@gmail.com>
2022-09-26 22:07:16 +0300

    Initialization the scoring attributes

--- scoreboard.py
+++ scoreboard.py
@@ -4,3 +4,8 @@ class Scoreboard():
     """Класс для виводу ігрової інформації"""
 
     def __init__(self, ai_game):
+        """Ініціалізує атрибути підрахунку поінтів"""
+        self.screen = ai_game.screen
+        self.screen_rect = self.screen.get_rect()
+        self.settings = ai_game.settings
+        self.stats = ai_game.stats
