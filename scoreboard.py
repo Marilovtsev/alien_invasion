41d1ce487095331b669b5263aa33ca5cb74012fc
Marilovtsev <dimarilovcev@gmail.com>
2022-09-26 22:17:47 +0300

    Account output on the right side of the screen.

--- scoreboard.py
+++ scoreboard.py
@@ -1,4 +1,5 @@
-import  pygame.font
+import pygame.font
+
 
 class Scoreboard():
     """Класс для виводу ігрової інформації"""
@@ -10,7 +11,7 @@ class Scoreboard():
         self.settings = ai_game.settings
         self.stats = ai_game.stats
 
-        #Налаштування шрифту для вивода рахунку
+        # Налаштування шрифту для вивода рахунку
         self.text_color = (30, 30, 30)
         self.font = pygame.font.SysFont(None, 40)
         # Підготовка початкового зображення
@@ -22,3 +23,7 @@ class Scoreboard():
         self.score_image = self.font.render(score_str, True,
                                             self.text_color, self.settings.bg_color)
 
+        # Вивід рахунку у правій частині екрану.
+        self.score_rect = self.score_image.get_rect()
+        self.score_rect.right = self.screen_rect.right - 20
+        self.score_rect.top = 20
