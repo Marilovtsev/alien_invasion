acf123063a768db783f689a6b12294174db58e29
Marilovtsev <dimarilovcev@gmail.com>
2022-09-28 21:24:32 +0300

    The record is aligned in the center of the upper side

--- scoreboard.py
+++ scoreboard.py
@@ -35,8 +35,12 @@ class Scoreboard():
         high_score = round(self.stats.high_score, -1)
         high_score_str = "{:,}".format(high_score)
         self.high_score_image = self.font.render(high_score_str, True,
-                                self.text_color, self.ai_settings.bg_color)
+                                                 self.text_color, self.ai_settings.bg_color)
 
+        # Рекорд вирівнюється по центру верхньої сторони
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.score_rect.top
 
     def show_score(self):
         """Виводить рахунок на екран"""
