aed45133dddcf61247a89aa9ba15ef9ca0040ae4
Marilovtsev <dimarilovcev@gmail.com>
2022-09-28 21:10:27 +0300

    Account rounding

--- scoreboard.py
+++ scoreboard.py
@@ -19,7 +19,8 @@ class Scoreboard():
 
     def prep_score(self):
         """Преобразує нинішній рахунок у графічне зображення"""
-        score_str = str(self.stats.score)
+        rounded_score = round(self.stats.score, -1)
+        score_str = "{:,}".format(rounded_score)
         self.score_image = self.font.render(score_str, True,
                                             self.text_color, self.settings.bg_color)
 
