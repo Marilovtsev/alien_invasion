ae77a7376b19a0aa0ec294a8ad3ac10733de6f14
Marilovtsev <dimarilovcev@gmail.com>
2022-09-28 21:25:16 +0300

    The record is aligned in the center of the upper side

--- scoreboard.py
+++ scoreboard.py
@@ -45,3 +45,4 @@ class Scoreboard():
     def show_score(self):
         """Виводить рахунок на екран"""
         self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
