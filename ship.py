30f3954b97d364ad9f3cb3d2b33f64e806e1be18
Marilovtsev <dimarilovcev@gmail.com>
2022-10-03 20:44:31 +0300

    Ship imitate from Sprite

--- ship.py
+++ ship.py
@@ -1,11 +1,13 @@
 import pygame
+from pygame.sprite import Sprite
 
 
-class Ship():
+class Ship(Sprite):
     """Класс для керування кораблем"""
 
     def __init__(self, ai_game):
         """Ініціалізує корабель та задає його початкову позицію"""
+        super().__init__()
         self.screen = ai_game.screen
         self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
